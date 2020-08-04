23b8cd623c2f97bde21a83aa5ec77c5993596969
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 03:22:42 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,10 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   #accepts_nested_attributes_for :categories
-#bi directional has many through 
+#bi directional has many through
 
+
+#customize the way our category is created by making this method 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
